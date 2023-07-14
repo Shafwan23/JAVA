0958112be75fb0fbbe7c76d821064e46d2f05140
Shafwan Ahmed <116721567+Shafwan23@users.noreply.github.com>
2023-07-14 23:59:24 +0530

    Add files via upload

---
+++ OOPS/Inheritance.java
@@ -0,0 +1,20 @@
+package OOPS;
+
+class A{
+	public void show(){
+		System.out.println("Lol");
+	}
+}
+class B extends A{
+	
+}
+public class Inheritance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		A a = new A();
+		a.show();
+	
+	}
+
+}
