d0e31f19834596a671c772048762258f183426d7
Shafwan Ahmed <116721567+Shafwan23@users.noreply.github.com>
2023-07-25 19:41:30 +0530

    Excep6.java

--- ExceptionHandling/Excep6.java
+++ ExceptionHandling/Excep6.java
@@ -16,20 +16,3 @@ public class Excep6 {
 
 }
 
-/*
-usage of wrapper class
-1. valueOf() method
-We can use the valueOf() method to create a Wrapper object for a given primitive or String.
-ex: Integer i1 = Integer.valueOf("10"); //10
-    Integer i2 = Integer.valueOf(10): //10
-
-2. xxxValue() method
-We can use xxxValue() methods to get the primitive for the given Wrapper Object. 
-ex: 
-
-3. parsexxx() method
-We can use parsexxx() methods to convert String to primitive.
- 
-4. toString() method
-We can use the toString() method to convert the Wrapper object or primitive to String. 
-*/
